67971718a12bb02b8cfe9c9117dd15e16e5386fb
Md Asmaul Sarder <mdasmaulsarder123@gmail.com>
2026-03-02 17:09:39 +0600

    data

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/api/data", (req, res) => {
+  const data = {
+    message: "This is some data from the server",
+    timestamp: new Date(),
+  };
+  res.json(data);
 });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
